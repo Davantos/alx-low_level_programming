ace26ca25d761f21dece7bfef2dcdf56b7791fd6
dawit <dawitgirma377@gmail.com>
2022-12-12 10:29:33 -0800

    Hello world

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
-  /*main - prints the phrase*/
-int main (void)
+/**
+ * main - prints the phrase with proper gramm..
+ * Return: 0 if exited properly, non-zero otherwise
+ */
+int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
