d4635738bd38f9dcdf57a9b9f759afd7272e6b66
dawit <dawitgirma377@gmail.com>
2022-12-12 15:23:12 -0800

    Variable if ad else

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,7 +1,10 @@
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
+
 /**
- * main - prints the value o the number
+ * main - assigns a random number to int n everytime
+ * it executes, and prints it
  * Return: Always 0 (Success)
  */
 int main(void)
@@ -14,7 +17,7 @@ if (n > 0)
 printf("%d is positive\n", n);
 else if (n == 0)
 printf("%d is zero\n", n);
-else if ("%d is negative")
+else if (n < 0)
 printf("%d is negative\n", n);
 return (0);
 }
