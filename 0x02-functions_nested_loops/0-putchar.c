37fad893932267a07d260e096920d9d9b3a9effa
dawit <dawitgirma377@gmail.com>
2022-12-14 11:07:54 -0800

    Loop

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,6 +6,13 @@
 */
 int main(void)
 {
-printf("_putchar\n");
+_putchar("_\n");
+_putchar("p\n");
+_putchar("u\n");
+_putchar("t\n");
+_putchar("c\n");
+_putchar("h\n");
+_putchar("a\n");
+_putchar("r\n");
 return (0);
 }
