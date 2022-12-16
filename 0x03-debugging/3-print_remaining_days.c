85f3dd0c78af7fe18fd1525d604ef55ee2de5c72
dawit <dawitgirma377@gmail.com>
2022-12-16 12:03:08 -0800

    Debugging

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -10,12 +10,7 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-/**
-* leap year when it can be divided
-* by 100 and 400 or by 4 evenly
-* 0 will define that the number is
-* divided and no remain
-*/
+
 if ((year % 100 == 0 && year % 400 == 0) || (year % 4 == 0))
 {
 if (month > 2 && day >= 60)
